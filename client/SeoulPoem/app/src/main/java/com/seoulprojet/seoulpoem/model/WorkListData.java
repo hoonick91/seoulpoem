e1691f970d53576c5fab83c5176d9e35798852e6
jay <kjh338076@naver.com>
2017-10-02 18:43:37 +0900

    workd add vieww

---
+++ client/SeoulPoem/app/src/main/java/com/seoulprojet/seoulpoem/model/WorkListData.java
@@ -0,0 +1,21 @@
+package com.seoulprojet.seoulpoem.model;
+
+/**
+ * Created by junhee on 2017. 10. 2..
+ */
+
+public class WorkListData {
+    public int leftImg;
+    public int circleImg;
+    public String title;
+    public String content;
+
+    public WorkListData(int leftImg, int circleImg, String title, String content) {
+        this.leftImg = leftImg;
+        this.circleImg = circleImg;
+        this.title = title;
+        this.content = content;
+    }
+}
+
+
