1b55235d9cd8cea8a79f83371b98ec6577170263
Jinwook <sjw9650@gmail.com>
2017-10-06 23:38:20 +0900

    adjust bookmark and add article/simple

--- server/routes/bookmark.js
+++ server/routes/bookmark.js
@@ -10,29 +10,33 @@ const pool = require('../config/db_pool');
 
 
 // 작품담기에 담기
-router.post('/:idarticles', async (req, res, next) => {
+
+router.post('/:idarticles', async (req, res) => {
   try {
       var connection = await pool.getConnection();
       await connection.beginTransaction();
 
+      let email =req.headers.email;
 
-      let query1 = 'select pictures_idpictures, setting_idsettings, users_idusers from seoul_poem.articles where idarticles = ?';
+      let query1 = 'select articles.pictures_idpictures as idpic, articles.idarticles as ida, articles.users_idusers as iduser from seoul_poem.articles where idarticles = ?';
       let selected = await connection.query(query1, req.params.idarticles);
 
-      let bookmark= {
-        articles_idarticles : req.params.idarticles,
-        articles_pictures_idpictures : selected[0].pictures_idpictures,
-        articles_users_idusers : selected[0].users_idusers,
-        articles_setting_idsettings : selected[0].setting_idsettings,
-        users_idusers: 1 //나중에 수정할것!
+      let query2 = 'select users.iduseers as users from seoul_poem.users where email = ?';
 
+      let userinfo = await connection.query(query2,email);
+
+      let bookmark= {
+          users_idusers: userinfo[0].users,
+          articles_idarticles : selected[0].ida,
+            articles_pictures_idpictures : selected[0].idpic,
+         articles_users_idusers : selected[0].iduser
       };
 
-      let query2 = 'insert into seoul_poem.bookmarks set ?';
-      await connection.query(query2, bookmark);
+      let query3 = 'insert into seoul_poem.bookmarks set ?';
+      await connection.query(query3, bookmark);
 
 
-      res.status(201).send({result: "bookmark success"});
+      res.status(201).json({status : "success", mag: "bookmark success"});
       await connection.commit();
 
     }
@@ -46,26 +50,26 @@ router.post('/:idarticles', async (req, res, next) => {
 });
 
 // 사용자 아이디를 이용하여 담은 작품 보기
-router.get('/search', async (req, res, next) => {
+router.get('/search', async (req, res) => {
   try {
       var connection = await pool.getConnection();
       await connection.beginTransaction();
 
-      //userid로 변경할 것!!!
-      let query1 = 'SELECT articles_idarticles FROM seoul_poem.bookmarks where users_idusers = 1';
-      var selected = await connection.query(query1);
-      console.log(selected[0].articles_idarticles);
-      console.log(selected);
+      let email = req.headers.email;
 
-      //picture 문제 해결하면 추가할 것!!
-      let query2 = 'SELECT idarticles, profile, poem.title, poem.content FROM seoul_poem.articles, seoul_poem.poem , seoul_poem.users ,seoul_poem.pictures where idarticles = ?  and articles.poem_idpoem = poem.idpoem and articles.users_idusers = users.idusers and articles.pictures_idpictures = pictures.idpictures;';
+      //userid로 변경할 것!!!
+      let query1 = 'SELECT seoul_poem.bookmarks.articles_idarticles as idarticles FROM seoul_poem.seoul_poem.bookmarks where seoul_poem.users.email = ? and seoul_poem.bookmarks.users_idusers = seoul_poem.users.idusers';
+      var selected = await connection.query(query1,email);
+
+      let query2 = 'SELECT articles.idarticles, pictures.photo, users.profile, articles.title FROM seoul_poem.articles, seoul_poem.users ,seoul_poem.pictures where idarticles = ?  and articles.users_idusers = users.idusers and articles.pictures_idpictures = pictures.idpictures;';
       let bookmark_list=[];
 
-      for(var i=0; i<selected.length; i++){
-        bookmark_list[i] = await connection.query(query2, selected[i].articles_idarticles);
+      let len_ = selected.length;
+      for(var i=0; i<len_; i++){
+        bookmark_list[i] = await connection.query(query2, selected[i].idarticles);
       }
 
-      res.status(200).send({bookmark_list: bookmark_list});
+      res.status(200).json({status : "success", bookmark_list: bookmark_list});
       //res.status(200).send({result: "bookmark search success"});
       await connection.commit();
 
--- server/routes/main.js
+++ server/routes/main.js
@@ -9,21 +9,25 @@ const jwt = require('jsonwebtoken');
         await connection.beginTransaction();
 
         req.checkQuery({tag : {notEmpty: true,errorMessage : 'error message'}});
-        var errors = req.validationErrors();
+        let errors = req.validationErrors();
         if (errors) {
-            res.status(501).send(errors);
+            res.status(501);
+            res.json({status : "fail",msg:err});
             return;
         }
         else{
-        let tag = '%'+req.query.tag+'%';
-        let query1 = 'select * from seoul_poem.pictures, seoul_poem.articles where seoul_poem.articles.pictures_idpictures=seoul_poem.pictures.idpictures and seoul_poem.articles.tags like ? order by rand() limit 5;'
-        let main_list = await connection.query(query1,tag);
-        res.status(200).send( { main_list : main_list });
-        await connection.commit();
+        let tag = '#'+req.query.tag+' ';
+        let query1 = 'select seoul_poem.pictures.photo,seoul_poem.articles.idarticles,seoul_poem.articles.title from seoul_poem.pictures, seoul_poem.articles where seoul_poem.articles.pictures_idpictures=seoul_poem.pictures.idpictures and seoul_poem.articles.tags like ? order by rand() limit 5;'
+        let main_list   = await connection.query(query1,tag);
+
+            res.status(200);
+            res.json({status:"success", data: main_list});
+            await connection.commit();
         }
     }catch(err){
         console.log(err);
-        res.status(500).send( { result: err });
+        res.status(500);
+        res.json({status : "fail",msg:err});
         connection.rollback();
 
     }finally{
@@ -41,21 +45,25 @@ router.get('/all', async ( req, res) => {
 
         req.checkQuery({tag : {notEmpty: true,errorMessage : 'error message'}});
 
-        var errors = req.validationErrors();
+        let errors = req.validationErrors();
         if (errors) {
-            res.status(501).send(errors);
+            res.status(501);
+            res.json({status : "fail",msg:err});
             return;
         } else {
-            let tag = '%' + req.query.tag+ '%';
-            let query1 = "select * from seoul_poem.pictures, seoul_poem.articles where seoul_poem.articles.pictures_idpictures=seoul_poem.pictures.idpictures and seoul_poem.articles.tags like ? order by seoul_poem.articles.idarticles Desc;"
+            let tag = '#' + req.query.tag+ ' ';
+            let query1 = "select seoul_poem.pictures.photo,seoul_poem.articles.idarticles from seoul_poem.pictures, seoul_poem.articles where seoul_poem.articles.pictures_idpictures=seoul_poem.pictures.idpictures and seoul_poem.articles.tags like ? order by seoul_poem.articles.idarticles Desc;"
             let main_list = await connection.query(query1, tag);
-            res.status(200).send({main_list: main_list});
+
+            res.status(200);
+            res.json({sucess:"success", data: main_list});
             await connection.commit();
         }
 
     }catch(err){
         console.log(err);
-        res.status(500).send( { result: err });
+        res.status(500);
+        res.json({status : "fail",msg:err});
         connection.rollback();
 
     }finally{
@@ -75,30 +83,75 @@ router.get('/search',async(req, res) => {
         await connection.beginTransaction();
         req.checkQuery({tag : {notEmpty: true,errorMessage : 'error message'}});
 
-        var errors = req.validationErrors();
+        let errors = req.validationErrors();
         if (errors) {
-            res.status(501).send(errors);
+            res.status(501);
+            res.json({status : "fail",msg:err});
             return;
         }
         else
         {
-        let tag ='%'+ req.query.tag+'%';
+            let tag ='%'+ req.query.tag+'%';
 
-        let query1 = "select * from seoul_poem.articles, seoul_poem.poem, seoul_poem.pictures where seoul_poem.articles.poem_idpoem=seoul_poem.poem.idpoem and seoul_poem.articles.pictures_idpictures=seoul_poem.pictures.idpictures and seoul_poem.articles.tags like ? order by seoul_poem.articles.idarticles Desc limit 3;"
-        let query2 = "select * from seoul_poem.users where seoul_poem.users.pen_name like ? and seoul_poem.users.author = 1 order by seoul_poem.users.idusers Desc limit 3"
+            let query2 = "select seoul_poem.users.pen_name as pen_name,seoul_poem.users.idusers as idusers,seoul_poem.users.profile as profile from seoul_poem.users where seoul_poem.users.pen_name like ? order by seoul_poem.users.idusers Desc limit 3"
+            let author_list = await connection.query(query2,tag);
 
-        let article_list = await connection.query(query1,tag);
-        let author_list = await connection.query(query2,tag);
+            let counts_author = author_list.length;
 
-        res.status(200).send( { author_list : author_list , article_list : article_list});
+            let i;
+            let query3 = "select count(*) as count from seoul_poem.articles, seoul_poem.poem where seoul_poem.articles.users_idusers = ? and seoul_poem.articles.poem_idpoem = seoul_poem.poem.idpoem";
+            let query4 = "select count(*) as count from seoul_poem.articles, seoul_poem.pictures where seoul_poem.articles.users_idusers = ? and seoul_poem.articles.pictures_idpictures = seoul_poem.pictures.idpictures";
+
+            let arr1 = [];
+
+            for (i=0;i<counts_author;i++){
+                let author = {};
+                author.name_ = author_list[i].pen_name;
+                let search_key = author_list[i].idusers;
+                author.profile =author_list[i].profile;
+                let article_count = await connection.query(query3,search_key);
+                author.ac = article_count[0].count;
+                let poem_count =  await connection.query(query4,search_key);
+                author.pc = poem_count[0].count;
+                arr1[i] = author;
+         }
+
+            let query1 = "select seoul_poem.articles.idarticles as idarticles,seoul_poem.articles.title as title,seoul_poem.articles.poem_idpoem as idpoem from seoul_poem.articles, seoul_poem.pictures where seoul_poem.articles.pictures_idpictures=seoul_poem.pictures.idpictures and seoul_poem.articles.tags like ? order by seoul_poem.articles.idarticles Desc limit 3;"
+
+            let article_list = await connection.query(query1,tag);
+
+            let counting = article_list.length;
+
+            let arr2 = [];
+
+            let query5 = "select seoul_poem.poem.content as content from  seoul_poem.poem where seoul_poem.poem.idpoem = ?";
+            console.log(counting);
+
+            for(i=0 ; i<counting;i++){
+                let articlelist= {};
+                articlelist.idarticles = article_list[i].idarticles;
+                articlelist.title = article_list[i].title;
+                if (article_list[i].idpoem == null){
+                    articlelist.contents=""
+                }
+                else {
+                    let poem_ = await connection.query(query5,article_list[i].idpoem);
+                    articlelist.contents= poem_[0].content;
+                }
+
+                arr2[i] = articlelist;
+            }
+        console.log(arr2);
+        res.status(200);
+        res.json( { status : "success",author_list : arr1 , article_list : arr2});
         await connection.commit();
         }
 
     }catch(err){
         console.log(err);
-        res.status(500).send( { result: err });
+        res.status(500);
+        res.json({status : "fail",msg:err});
         connection.rollback();
-
     }
     finally {
         pool.releaseConnection(connection);
